217a4b135838338aaca1090daf01491eb8106cc5
megablas <blas.r7@gmail.com>
2025-08-08 10:24:47 -0300

    Layaut nuevo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,26 @@
 import React, { ReactNode } from 'react';
 import AppHeader from './Header';
 import Footer from './Footer';
+import { useAuth } from '../contexts/AuthContext';
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const { isSuperUserMode } = useAuth();
+
     return (
         <div className="flex flex-col min-h-screen">
             <AppHeader />
             <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 {children}
             </main>
-            <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-               <Footer />
-            </div>
+            {!isSuperUserMode && (
+                <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                   <Footer />
+                </div>
+            )}
         </div>
     );
 }
